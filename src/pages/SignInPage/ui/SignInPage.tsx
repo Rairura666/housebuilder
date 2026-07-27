918552be4158d754dd250b01710042e25ba88805
Rairura <biryukova.ira63@gmail.com>
2026-07-27 13:09:30 +0300

    feat: improve SignIn and SignUp pages

--- src/pages/SignInPage/ui/SignInPage.tsx
+++ src/pages/SignInPage/ui/SignInPage.tsx
@@ -1,25 +1,37 @@
+import { Link } from 'react-router-dom'
 import { useSignIn } from '../model/useSignIn'
+import "./SignInPage.css"
 
 export const SignInPage = () => {
 
     const { email, setEmail, password, setPassword, handleSignInSubmit } = useSignIn()
 
     return (<>
-        <input
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="email"
-        />
+        <div className="signInWrapper">
+            <div className="signInContent">
+                <input
+                    type='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                />
 
-        <input
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-        />
+                <input
+                    placeholder="Password"
+                    type='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
 
-        <button
-            onClick={handleSignInSubmit}
-        >sign in</button>
+                <button
+                    onClick={handleSignInSubmit}
+                >Sign in</button>
+
+                <div className="signInRedirectToSignUp">
+                    <span>Don't have an account yet?</span>
+                    <Link to="/signup">Sign up</Link>
+                </div>
+            </div>
+        </div>
     </>)
 }
--- src/pages/SignUpPage/ui/SignUpPage.tsx
+++ src/pages/SignUpPage/ui/SignUpPage.tsx
@@ -1,25 +1,44 @@
+import { Link } from 'react-router-dom'
 import { useSignup } from '../model/useSignUp'
+import "./SignUpPage.css"
 
 export const SignUpPage = () => {
-        
-    const {email, setEmail, password, setPassword, handleSignUpSubmit} = useSignup()
+
+    const { email, setEmail, password, setPassword, handleSignUpSubmit } = useSignup()
 
     return (<>
-    <input
-    type='email'
-    value={email}
-    onChange={(e) => setEmail(e.target.value)}
-    placeholder="email"
-    />
+        <div className="signUpWrapper">
+            <div className="signUpContent">
+                <input
+                    type='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                />
 
-    <input
-    placeholder="password"
-    value={password}
-     onChange={(e) => setPassword(e.target.value)}
-    />
+                <input
+                    type='text'
+                    value={""}
+                    onChange={()=>{}}
+                    placeholder='Username'
+                />
 
-    <button
-    onClick={handleSignUpSubmit}
-    >sign up</button>
+                <input
+                    placeholder="Password"
+                    type='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+
+                <button
+                    onClick={handleSignUpSubmit}
+                >Sign up</button>
+
+                <div className="signInRedirectToSignIn">
+                    <span>Already have an account?</span>
+                    <Link to="/signin">Sign in</Link>
+                </div>
+            </div>
+        </div>
     </>)
 }
